51fdd1b0a7b1ec13875930402cfbcad6182d3765
Tushar Asthana <tasthana@outlook.com>
2024-02-27 21:21:35 -0500

    PlayerCharacter class + functions w/docs

--- player.py
+++ player.py
@@ -8,6 +8,14 @@ class PlayerCharacter(arcade.Sprite):
         current_item_slot_selected, health, and stamina
 
         """
+        super().__init__()
+
+        # Inventory attributes
+        self.inventory = [None, None, None, None]  # Assuming 4 inventory slots
+        self.holding_two_handed = False
+        self.current_item_slot_selected = 1  # Default to first slot
+        self.health = 100
+        self.stamina = 100
 
         """
         current item slot selected:
@@ -22,32 +30,70 @@ class PlayerCharacter(arcade.Sprite):
         :return:
         """
 
-"""
-Needed functions:
-get_health() - returns health value
-get_stam() - returns stamina value
+# get_health() - returns health value
+def get_health(self):
+    return self.health
 
+# get_stam() - returns stamina value
+def get_stam(self):
+    return self.stamina
+
+"""
 get_inv(int) - returns the boolean value of the inputted inventory slot (true if something in it, false if not)
 - return false if player is holding a two-handed item (holding_two_handed is true), even if the other slot is full
 - this will be 1 indexed: first inventory slot is 1, second is 2, etc
+"""
+def get_inv(self, inventory_slot):
+    # Check if holding a two-handed item
+    if self.holding_two_handed:
+        return False
+    # Adjust for 1-indexed slots
+    slot_index = inventory_slot - 1
+    return self.inventory[slot_index] is not None
 
-set_two_handed(bool) set two handed
+# set_two_handed(bool) set two handed
+def set_two_handed(self, is_two_handed):
+    self.holding_two_handed = is_two_handed
 
+"""
 add_item(inventory_slot, item)
 - you don't need to handle for the inventory slot being open or closed
 - item will be an instance of the Item class, simply update the index of inventory_slot - 1 with this item
+"""
+def add_item(self, inventory_slot, item):
+    # Adjust for 1-indexed slots
+    slot_index = inventory_slot - 1
+    self.inventory[slot_index] = item
 
+"""
 remove_item(inventory_slot)
 - remove and return the object of the index of inventor_slot - 1, then set that index to be None (or whatever the 
   default empty value you decide for it) (the x and y coordinates of the item object need to be updated to the player's current location
   (This should be some sort of call to self.center_x and self.center_y to access the player Sprite's center
+  """
+def remove_item(self, inventory_slot):
+    # Adjust for 1-indexed slots
+    slot_index = inventory_slot - 1
+    removed_item = self.inventory[slot_index]
+    self.inventory[slot_index] = None
+    return removed_item
 
-decrease_stam(int), decrease_health(int)
+def decrease_stam(self, amount):
+    self.stamina -= amount
+    if self.stamina < 0:
+        self.stamina = 0
 
-get_stam(), get_health()
+def decrease_health(self, amount):
+    self.health -= amount
+    if self.health < 0:
+        self.health = 0
 
-add_stam(), add_health()
-(there are some health mechanics and cases for adding it)
+def add_stam(self, amount):
+    self.stamina += amount
+    if self.stamina > 100:  # Assuming max stamina is 100
+        self.stamina = 100
 
-
-"""
+def add_health(self, amount):
+    self.health += amount
+    if self.health > 100:  # Assuming max health is 100
+        self.health = 100
