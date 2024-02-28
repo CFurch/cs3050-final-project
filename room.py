0e7451d1c5f1c2136930e3b04179ca43ae408ff3
Jason Tomasi <jason.tomasi@uvm.edu>
2024-02-28 16:07:18 -0500

    Room updates, added Init, Setup, and get_walls()

--- room.py
+++ room.py
@@ -7,15 +7,35 @@ class Room(arcade.Sprite):
         Spawn areas (coordinates for bounding boxes (x, y), list of list of tuples), room type,
         doors, loot_item_spawn_list, wall_list
         """
+        self.wall_list = None
+        self.loot_item_spawn_list = None
+        self.spawners = None
+        self.room_type = None
+        self.doors = None
+        self.x_center = None
+        self.y_center = None
+        self.hazards = None
+        self.spawners = None
 
-    def setup(self, room_type, item, x_top_left, y_top_left, spawners, hazards):
+    def setup(self, room_type, item, x_center, y_center, spawners=None, hazards=None, loot_item_spawn_list = None):
         """
         loot_value, allow null for spawners and hazards to be null
         :return:
         """
+        self.wall_list = arcade.SpriteList()
+        self.loot_item_spawn_list = loot_item_spawn_list
+        self.spawners = spawners
+        self.hazards = hazards
+        self.room_type = room_type 
+        self.x_center = x_center
+        self.y_center = y_center
+    def get_walls():
+        return self.wall_list    
 
 """
 This class needs a get_walls method to return the walls of the room in the form of a list.
 """
+
+
 
 
